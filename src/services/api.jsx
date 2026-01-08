c65c9bb9b497d0248271d285bdb69dbddd3f8677
dhruv263 <dhruvchavda3232@gmail.com>
2026-01-08 15:11:58 +0530

    Update API base URL for production

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://mern-auth-backend-db8j.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
